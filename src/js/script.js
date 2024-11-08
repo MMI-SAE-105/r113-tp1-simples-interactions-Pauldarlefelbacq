f18cd08e6b0124e7837730e2c5d123b8b8d8f1d5
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2024-11-08 10:37:25 +0100

    déclaration des constantes

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 // TODO
 //Ex 1
 
-var aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
 aMettreEnRouge.style.color ="red";
 
 //Ex2
 
-enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 enRougeSuiteAClick.addEventListener("click", (evt) =>{
     enRougeSuiteAClick.syle.color ="red";
 })
 
 //Ex3
 
-collH2 = document.querySelectorAll("H2");
+const collH2 = document.querySelectorAll("H2");
 collH2.forEach((elm)=>{
     elm.addEventListener("click", (evt)=>{
         evt.target.style.color ="red";
